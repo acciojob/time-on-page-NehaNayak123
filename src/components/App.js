4d0410bb5c8d8878e2a0c42f5d31f07357ac4469
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-04-13 23:54:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 
-import React from "react";
-import {useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
-const App = () => {
+function App(){
   const [count, setCount]=useState(0);
   useEffect(()=>{
     const interval=setInterval(()=>{
@@ -21,23 +20,4 @@ const App = () => {
 }
 
 export default App
-
-///////
-import {useState, useEffect } from "react"
-export default function App() {
-    const [count , setCount]=useState(0)
-    useEffect(()=>{
-        const interval= setInterval(()=>{
-            setCount((c)=>c+1)
-        },1000)
-
-        return ()=> clearInterval(interval)
-    },[])
-
-    
-  return <div>
-      <h1>Hello world</h1>
-      <h3>Count: {count}</h3>
-  </div>
-}
 

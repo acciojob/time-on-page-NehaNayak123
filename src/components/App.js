810c8ec171d1d46bb4555010e11000c5ba70b357
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-04-13 23:43:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 
 import React from "react";
+import {useState, useEffect } from "react"
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount]=useState(0);
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+      setCount((c)=>c+1)
+    },1000)
+
+    return ()={
+      clearInterval(interval)
+    }
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
+      <p>`You have been on this page for {count} seconds.`</p>
     </div>
   )
 }
 
 export default App
+
+///////
+import {useState, useEffect } from "react"
+export default function App() {
+    const [count , setCount]=useState(0)
+    useEffect(()=>{
+        const interval= setInterval(()=>{
+            setCount((c)=>c+1)
+        },1000)
+
+        return ()=> clearInterval(interval)
+    },[])
+
+    
+  return <div>
+      <h1>Hello world</h1>
+      <h3>Count: {count}</h3>
+  </div>
+}
+
